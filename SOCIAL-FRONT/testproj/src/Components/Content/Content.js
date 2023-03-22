1aa6ede7d924ff4f297e654198c22113ae9a9bcb
unknown <glepsgulin@gmail.com>
2023-03-22 16:13:54 +0200

    some models

--- SOCIAL-FRONT/testproj/src/Components/Content/Content.js
+++ SOCIAL-FRONT/testproj/src/Components/Content/Content.js
@@ -1,9 +1,12 @@
+import UserPosts from './UserPosts/UserPosts';
 import classes from './Content.module.css';
+
 const MainContent = () => {
     return(
         <div className={classes.MainContent}>
             <p>Will be main content</p>
             <img src="https://images.pexels.com/photos/1001682/pexels-photo-1001682.jpeg?auto=compress&cs=tinysrgb&w=600"/>
+            <UserPosts />
         </div>
     );
 }
---
+++ SOCIAL-FRONT/testproj/src/Components/Content/UserPosts/UserPosts.js
@@ -0,0 +1,13 @@
+const UserPosts = () => {
+    return (
+        <div className="">
+            <div><p>First Post 1</p></div>
+            <div><p>First Post 2</p></div>
+            <div><p>First Post 3</p></div>
+            <div><p>First Post 4</p></div>
+            <div><p>First Post 5</p></div>
+        </div>
+    );
+}
+
+export default UserPosts;
